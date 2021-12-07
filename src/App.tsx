a4701dc8030952857b826b3dbeb3d5dbb1e89e04
Art <arthurluiz36@gmail.com>
2021-12-07 18:31:35 -0200

    Criado o escopo da area da aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from './styles'
 
-function App() {
+const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container> 
+        <C.Area>
+          <C.Header> Galeria de fotos </C.Header>
+
+
+          {/* Area de upload */}
+
+
+          {/* Lista das Fotos */}
+        </C.Area>
+    </C.Container>
   );
 }
 
