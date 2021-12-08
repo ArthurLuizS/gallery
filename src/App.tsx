e46b38993ce834d0658dad0f146c2e48f09ccc78
Art <arthurluiz36@gmail.com>
2021-12-08 18:52:03 -0200

    usando states para esperar a comunicação do gettAll e colocando avisos de carregamento e mensagem de que nao ha fotos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import * as C from './styles'
+import {useState, useEffect} from 'react'
+import * as Photos from './services/photo'
+import {Photo} from './types/photo'
 
 const App = () =>{
+  const [loading, setLoading] = useState(false)
+  const [photos, SetPhotos] = useState<Photo[]>([]);
+
+  useEffect(()=>{
+    const getPhotos = async () =>{
+      setLoading(true);
+      SetPhotos(await Photos.getAll());
+      setLoading(false);
+    }
+    getPhotos();
+  }, [])
+
   return (
     <C.Container> 
         <C.Area>
@@ -11,6 +26,27 @@ const App = () =>{
 
 
           {/* Lista das Fotos */}
+
+          {loading &&
+            <C.ScreenWarning> 
+              <div className="emoji"> 🧙 ⌛ </div>
+              <div> Carregando....</div>
+            </C.ScreenWarning>
+          }
+
+          {!loading && photos.length > 0 &&
+            <C.PhotoList>
+              {photos.map((item, index) =>(
+                <div> {item.name} </div>
+              ))}
+            </C.PhotoList>
+          } 
+          {!loading && photos.length === 0 &&
+             <C.ScreenWarning> 
+              <div className="emoji"> 🤷‍♂️  </div>
+              <div> Ainda nao há fotos cadastradas...</div>
+           </C.ScreenWarning>
+          }
         </C.Area>
     </C.Container>
   );
